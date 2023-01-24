5698eb01b88031e157acdf164c8b22ee09baf54c
Clinton <clintogechi@gmail.com>
2023-01-24 18:14:27 +0300

    World time app that consumes time api

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading_screen.dart';
+
+void main() {
+  runApp(MaterialApp(initialRoute: '/home', routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  }));
+}
